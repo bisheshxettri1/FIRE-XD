c46799354bb1a1ce246b9dfcb9b7cbc9426abc70
bisheshxettri1 <154910016+bisheshxettri1@users.noreply.github.com>
2025-09-09 09:51:07 +0545

    Update run.py

--- run.py
+++ run.py
@@ -1,5 +1,8 @@
-import importlib.util
+import RUN
 
-spec = importlib.util.spec_from_file_location("RUN", "./RUN.so")
-RUN = importlib.util.module_from_spec(spec)
-spec.loader.exec_module(RUN)
+if __name__ == "__main__":
+    # If RUN.so has a menu() function, call it
+    try:
+        RUN.menu()
+    except AttributeError:
+        print("RUN module loaded, but no menu() function found.")
