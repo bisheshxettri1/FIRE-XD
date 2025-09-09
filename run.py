3fb7856c47bba4fb6a3a8b17f503991f9a0c4df1
bisheshxettri1 <154910016+bisheshxettri1@users.noreply.github.com>
2025-09-09 09:59:27 +0545

    Update run.py

--- run.py
+++ run.py
@@ -2,4 +2,12 @@ import importlib.machinery
 
 RUN = importlib.machinery.ExtensionFileLoader("o_enc", "RUN.so").load_module()
 
-print(dir(RUN))
+if __name__ == "__main__":
+    # Try running the main function directly
+    try:
+        RUN.menu()
+    except AttributeError:
+        try:
+            RUN.main()
+        except AttributeError:
+            print("Module loaded, but no menu() or main() function found.")
