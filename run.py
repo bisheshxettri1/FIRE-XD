859dc9fec7971c2655f02cde563177ebfa0e59d9
bisheshxettri1 <154910016+bisheshxettri1@users.noreply.github.com>
2025-09-09 09:49:00 +0545

    Update run.py

--- run.py
+++ run.py
@@ -1,2 +1,5 @@
-#bishesheyy the greatt
-import RUN
+import importlib.util
+
+spec = importlib.util.spec_from_file_location("RUN", "./RUN.so")
+RUN = importlib.util.module_from_spec(spec)
+spec.loader.exec_module(RUN)
